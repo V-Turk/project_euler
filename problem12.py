d8dc867c02ff8bc840b061de825c84da25430b3b
V-Turk <velimir94@gmail.com>
2022-09-13 21:12:41 +0200

    problem 12

--- problem12.py
+++ problem12.py
@@ -1,22 +1,18 @@
-import math
-
-a = [1,3,6,10]
-b = []
-k = 0
-while k == 0:
-	for i in range(5,1000000):
-		j = a[-1] + i
-		a.append(j)
-		count = 1
-		if j%10 == 0:
-			for l in range(1,int(j/2)+1):
-				if j % l == 0:
-					count += 1
-					if count >= 500:
-						k = 1
-			print(j,count)
-
-print(j)
-
-
-
+a = [];
+i = 1;
+j = 1;
+x = 0;
+b = [];
+while i < 2:
+    b = [];
+    x = x + j;
+    for k in range(1,int(x**(1/2))+1):
+        if x % k == 0:
+            b.append(k)
+            if x // k != k:
+                b.append(x // k)
+    print(j,x,len(b))
+    if len(b) >= 500:
+        i += 1;
+        print("|||||    ",x, len(b))
+    j += 1;
